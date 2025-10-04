51305e99f8eee6979f4b3a30991593a1b3509861
sachinrastogi7777 <sachinrastogi23101997@gmail.com>
2025-10-04 06:59:16 +0530

    Added API for getting connection and feed page. Also added pagination in the feed API.

--- LinkUp BE/app.js
+++ LinkUp BE/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const authRouter = require('./src/router/auth');
 const profileRouter = require('./src/router/profile');
 const requestRouter = require('./src/router/request');
+const userRouter = require('./src/router/user');
 const app = express();
 
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(cookieParser());
 app.use('/', authRouter);
 app.use('/', profileRouter);
 app.use('/', requestRouter);
+app.use('/', userRouter);
 
 connectDb().then(() => {
     console.log("Database connected successfully...");
--- LinkUp BE/src/router/user.js
+++ LinkUp BE/src/router/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userAuth = require('../middleware/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
 const userRouter = express.Router();
 
@@ -35,6 +36,67 @@ userRouter.get('/user/requests/sent', userAuth, async (req, res) => {
     }
 });
 
+userRouter.get('/user/connections', userAuth, async (req, res) => {
+    const USER_SAFE_DATA = ['firstName', 'lastName', 'gender', 'dateOfBirth', 'interests', 'about', 'profileImage', 'location', 'userName', 'coverImage', 'createdAt', 'updatedAt']
+    try {
+        const loggedInUser = req.user;
+        const totalConnection = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id, status: 'accepted' },
+                { toUserId: loggedInUser._id, status: 'accepted' }
+            ]
+        }).populate('fromUserId', SAFE_DATA).populate('toUserId', USER_SAFE_DATA);
 
+        const userConnection = totalConnection.map((connection) => {
+            if (connection.fromUserId.equals(loggedInUser._id)) {
+                return { connectionId: connection._id, connectedAt: connection.updatedAt, user: connection.toUserId }
+            } else {
+                return { connectionId: connection._id, connectedAt: connection.updatedAt, user: connection.fromUserId }
+            }
+        });
+        if (userConnection.length == 0) {
+            res.json({ message: "You Don't have any Connection." })
+            return
+        }
+        res.json({ userConnection });
+    } catch (error) {
+        res.status(500).json({ "ERROR": error.message });
+    }
+});
+
+userRouter.get('/feed', userAuth, async (req, res) => {
+    const SAFE_DATA = ['firstName', 'lastName', 'interests', 'about', 'profileImage', 'location', 'userName', 'createdAt']
+    try {
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 25 ? 25 : limit;
+        const skip = (page - 1) * limit;
+
+        // Find all connection requests (Sent + Recieved)
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select(['fromUserId', 'toUserId']);
+
+        const hideUserFromFeed = new Set();
+        connectionRequest.map((connection) => {
+            hideUserFromFeed.add(connection.fromUserId.toString());
+            hideUserFromFeed.add(connection.toUserId.toString());
+        });
+
+        const userToShowOnFeed = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUserFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(SAFE_DATA).skip(skip).limit(limit);
+        res.json({ data: userToShowOnFeed })
+    } catch (error) {
+        res.status(500).json({ "ERROR": error.message });
+    }
+})
 
 module.exports = userRouter;
